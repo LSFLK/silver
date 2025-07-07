7dc1f7ed32c2ee2ec76a6979f7403c4df977cbf7
Maneesha Walisundera <maneesha@opensource.lk>
2025-07-07 11:00:31 +0530

    chore: housekeeping and dovecot service.
     * fixed tls naming in smtp and dovecot folders.
    * Added dovecot service to compose.

--- services/smtp/conf/docker-entrypoint.sh
+++ services/smtp/conf/docker-entrypoint.sh
@@ -8,8 +8,8 @@ echo "INFO: Initializing Postfix configuration..."
 postconf -e "myhostname=${MYHOSTNAME}"
 postconf -e "mydestination=${MYDESTINATION}"
 postconf -e "inet_interfaces=${INET_INTERFACES}"
-postconf -e "smtpd_tls_cert_file=/etc/postfix/ssl/fullchain.pem"
-postconf -e "smtpd_tls_key_file=/etc/postfix/ssl/privkey.pem"
+postconf -e "smtpd_tls_cert_file=/etc/postfix/tls/fullchain.pem"
+postconf -e "smtpd_tls_key_file=/etc/postfix/tls/privkey.pem"
 
 echo "INFO: Running 'postfix check'..."
 
