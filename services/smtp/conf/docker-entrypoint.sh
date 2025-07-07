cc5ad385d2ec3c62447a01c5c6040771917c73f7
Maneesha Walisundera <maneesha@opensource.lk>
2025-07-07 15:28:03 +0530

    fix: added initial dovecot sasl settings and fixed hostname issue.

--- services/smtp/conf/docker-entrypoint.sh
+++ services/smtp/conf/docker-entrypoint.sh
@@ -5,11 +5,14 @@ set -e
 echo "INFO: Initializing Postfix configuration..."
 
 # postfix config 
-postconf -e "myhostname=${MYHOSTNAME}"
-postconf -e "mydestination=${MYDESTINATION}"
-postconf -e "inet_interfaces=${INET_INTERFACES}"
-postconf -e "smtpd_tls_cert_file=/etc/postfix/tls/fullchain.pem"
-postconf -e "smtpd_tls_key_file=/etc/postfix/tls/privkey.pem"
+postconf -e "myhostname = ${MYHOSTNAME}"
+postconf -e "mydestination = ${MYDESTINATION}"
+postconf -e "inet_interfaces = ${INET_INTERFACES}"
+postconf -e "smtpd_tls_cert_file = /etc/postfix/tls/fullchain.pem"
+postconf -e "smtpd_tls_key_file = /etc/postfix/tls/privkey.pem"
+postconf -e "smtpd_sasl_type = dovecot"
+postconf -e "smtpd_sasl_path = private/auth"
+postconf -e "smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
 
 echo "INFO: Running 'postfix check'..."
 
